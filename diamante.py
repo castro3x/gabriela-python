a08d2812a009c2986ccf420fbafaad49636f755f
castro3x <104210900+castro3x@users.noreply.github.com>
2022-05-13 09:59:21 -0400

    Create diamante.py

---
+++ diamante.py
@@ -0,0 +1,7 @@
+Rect(0, 0, 400, 400, fill=gradient('powderBlue', 'lightCyan', start='top'))
+Polygon(100,100,60,150,200,300,340,150,300,100, fill=gradient('white', 'lightBlue'), border='black', borderWidth=4)
+Polygon(125,150,200,300,275,150,200,100,fill='azure', border='black', borderWidth=2)
+Line(60,150,340,150, lineWidth=2)
+Line(100,100,125,150, lineWidth=2)
+Line(300,100,275,150, lineWidth=2)
+Star(265,120,15,6, fill='white', roundness=15)
